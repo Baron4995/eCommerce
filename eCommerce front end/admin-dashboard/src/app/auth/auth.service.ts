5392692835717e2b9527b12d4c8ae684238f5d31
Baron4995 <baron4995@outlook.com>
2023-10-16 11:40:08 -0700

    more edits for localhost

--- eCommerce front end/admin-dashboard/src/app/auth/auth.service.ts
+++ eCommerce front end/admin-dashboard/src/app/auth/auth.service.ts
@@ -55,7 +55,7 @@ export class AuthService {
   // }
 
   signIn(admin:Admin):Observable<String>{
-    return this.httpClient.post("http://50.18.247.45:8181/admin/signIn", admin, {responseType:'text'});
+    return this.httpClient.post("http://52.87.183.49:8181/admin/signIn", admin, {responseType:'text'});
   }
 
 
